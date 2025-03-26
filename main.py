5a8b9de1d08586ea15c2b777a31a73dfbc56278d
iFreaku <90742387+iFreaku@users.noreply.github.com>
2025-03-27 01:49:37 +0530

    update

--- main.py
+++ main.py
@@ -68,9 +68,9 @@ def ai(msg):
     return response.choices[0].message.content
 
 def up():
-    prompt = ai(f"the text ' {get_uptime()} ' in center, (any font style, any background or setting that you might think would be best here) (dont respnd with anything else other than the image gen prompt, you can even lightly imporve the prompt, just little subtle changes.)")
+    promp = ai(f"the text ' {get_uptime()} ' in center, (any font style, any background or setting that you might think would be best here) (dont respnd with anything else other than the image gen prompt, you can even lightly imporve the prompt, just little subtle changes.)")
     response = together_client.images.generate(
-        prompt=prompt,
+        prompt=promp,
         model="black-forest-labs/FLUX.1-schnell-Free",
         steps=4,
         n=1,
